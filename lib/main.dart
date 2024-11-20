c54f71a2cbc345f49c8f9b6c766908d8afff9b8b
GetuliovmSantos <getulio.vm.santos@gmail.com>
2024-11-20 09:31:43 -0300

    feat(App): adição Card para a palavra
    Criação de uma nova classe para a manipulação do card para ficar com a palavra, além disso foi centralizado a coluna com as duas informações(card e botão)

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
         // Tema do aplicativo usando Material 3
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.dark(),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Color.fromRGBO(0, 255, 0, 1.0),
+              ),
         ),
         // Página inicial do aplicativo
         home: MyHomePage(),
@@ -49,28 +51,75 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Obtém o estado do aplicativo
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+    final theme = Theme.of(context);
 
     return Scaffold(
       // Estrutura básica da página
-      body: Column(
-        children: [
-          // Texto fixo
-          Text('A random idea:'),
-          // Texto que exibe a palavra aleatória atual
-          Text(appState.current.asLowerCase),
-          // Botão para gerar a próxima palavra
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.blue)),
-            onPressed: () => appState.getNext(),
-            child: Text(
-              "Next",
-              style: TextStyle(
-                color: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Exibe a palavra aleatória atual em um cartão grande
+            BigCard(pair: pair),
+            SizedBox(
+              height: 10,
+            ),
+            // Botão para gerar a próxima palavra
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    WidgetStatePropertyAll(theme.colorScheme.secondary),
+              ),
+              onPressed: () => appState.getNext(),
+              child: Text(
+                "Next",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Widget que exibe a palavra aleatória em um cartão grande
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  // Par de palavras geradas aleatoriamente
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtém o tema atual do contexto
+    final theme = Theme.of(context);
+    
+    // Obtém o estilo de texto displayMedium e modifica a cor para onPrimary
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+    
+    return Card(
+      // Define a cor de fundo do Card como a cor primária do tema
+      color: theme.colorScheme.primary,
+      child: Padding(
+        // Define o padding interno do Card
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          // Exibe a palavra em formato PascalCase
+          pair.asPascalCase,
+          // Aplica o estilo de texto modificado
+          style: style,
+          // Define um rótulo semântico para acessibilidade
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
